1947cf584438089e05de1c5a3325773ed1a556d8
natnael340 <natnaeltilahun157@gmail.com>
2023-01-31 10:42:07 +0300

    Setup Tailwind on react native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
